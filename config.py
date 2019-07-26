e32cb1c87c0516593a7cc76416abd9e67adff65e
myeongjun <leaderj1001@gmail.com>
2019-07-26 15:44:54 +0900

    [Update] add distributed arguments

--- config.py
+++ config.py
@@ -29,11 +29,11 @@ def get_logger(filename):
 def get_args():
     parser = argparse.ArgumentParser('parameters')
 
-    parser.add_argument('--dataset', type=str, default='CIFAR10')
+    parser.add_argument('--dataset', type=str, default='CIFAR10', help='CIFAR10, CIFAR100, MNIST')
     parser.add_argument('--model-name', type=str, default='ResNet26', help='ResNet26, ResNet38, ResNet50')
     parser.add_argument('--img-size', type=int, default=32)
     parser.add_argument('--batch-size', type=int, default=25)
-    parser.add_argument('--num-workers', type=int, default=4)
+    parser.add_argument('--num-workers', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--lr', type=float, default=1e-1)
     parser.add_argument('--momentum', type=float, default=0.9)
@@ -41,7 +41,15 @@ def get_args():
     parser.add_argument('--print-interval', type=int, default=100)
     parser.add_argument('--cuda', type=bool, default=True)
     parser.add_argument('--pretrained-model', type=bool, default=False)
-    parser.add_argument('--stem', type=bool, default=False, help='attention: True, conv: False')
+    parser.add_argument('--stem', type=bool, default=False, help='attention stem: True, conv: False')
+
+    parser.add_argument('--distributed', type=bool, default=False)
+    parser.add_argument('--gpu-devices', type=int, nargs='+', default=None)
+    parser.add_argument('--gpu', type=int, default=None)
+    parser.add_argument('--rank', type=int, default=0, help='current process number')
+    parser.add_argument('--world-size', type=int, default=1, help='Total number of processes to be used (number of gpus)')
+    parser.add_argument('--dist-backend', type=str, default='nccl')
+    parser.add_argument('--dist-url', default='tcp://127.0.0.1:3456', type=str)
 
     args = parser.parse_args()
 
